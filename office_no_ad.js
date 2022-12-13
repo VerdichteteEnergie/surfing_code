e63fc95504fd9eec410f4a2e958b662689ee0403
VerdichteteEnergie <76449084+VerdichteteEnergie@users.noreply.github.com>
2022-12-13 03:53:10 +0100

    Create office_no_ad.js

---
+++ office_no_ad.js
@@ -0,0 +1,7 @@
+if (document.getElementById("show-templates").textContent == "Vorlagen ausblenden" || document.getElementById("show-templates").value == "Vorlagen ausblenden" || 		document.getElementById("show-templates").data-alt == "Vorlagen ausblenden") {
+	document.getElementById("show-templates").click()
+}
+
+if (document.getElementById("show-templates").textContent = "Vorlagen ausblenden" || document.getElementById("show-templates").value = "Vorlagen ausblenden" || 		document.getElementById("show-templates").data-alt = "Vorlagen ausblenden") {
+	document.getElementById("show-templates").click()
+}
